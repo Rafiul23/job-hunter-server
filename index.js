6e7a77a104cb86239b28a4546da15c40d70c9376
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-09-11 19:29:13 +0600

    get all categories from database is successful

--- index.js
+++ index.js
@@ -29,9 +29,19 @@ async function run() {
     const jobsCollection = client.db('JobDB').collection('jobs');
 
 
+    // get all categories
+    app.get('/categories', async(req, res)=>{
+        const cursor = categoryCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
