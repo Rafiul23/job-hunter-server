05d97db8540198b0d16ef906c9a981fdf062b051
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-09-14 20:01:49 +0600

    added a search api for searching jobs and added middlewares

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = 5000 || process.env.PORT;
+const cors = require('cors');
 
+// middlewares
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wlof2pa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -13,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -22,43 +26,61 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
-    const categoryCollection = client.db('JobDB').collection('categories');
-    const hotJobsCollection = client.db('JobDB').collection('hotjobs');
-    const jobsCollection = client.db('JobDB').collection('jobs');
-
+    const categoryCollection = client.db("JobDB").collection("categories");
+    const hotJobsCollection = client.db("JobDB").collection("hotjobs");
+    const jobsCollection = client.db("JobDB").collection("jobs");
 
     // get all categories
-    app.get('/categories', async(req, res)=>{
-        const cursor = categoryCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/categories", async (req, res) => {
+      const cursor = categoryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // get all hotjobs
-    app.get('/hotjobs', async(req, res)=>{
-        const cursor = hotJobsCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/hotjobs", async (req, res) => {
+      const cursor = hotJobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // get jobs by category
-    app.get('/jobs', async (req, res) => {
-        let query = {};
-        
-        if (req.query?.category) {
-            //  regex with the 'i' option for case-insensitive search
-            query = { category: { $regex: new RegExp(`^${req.query.category}$`, 'i') } };
-        }
-        
-        console.log(query);
-        const result = await jobsCollection.find(query).toArray();
-        
-        res.send(result);
-    });
-    
+    app.get("/jobs", async (req, res) => {
+      let query = {};
 
+      if (req.query?.category) {
+        //  regex with the 'i' option for case-insensitive search
+        query = {
+          category: { $regex: new RegExp(`^${req.query.category}$`, "i") },
+        };
+      }
+
+      // console.log(query);
+      const result = await jobsCollection.find(query).toArray();
+
+      res.send(result);
+    });
+
+    // search by job title
+    app.get("/search", async (req, res) => {
+      let query = {};
+
+      if (req.query?.title) {
+        //  regex with the 'i' option for case-insensitive search
+        query = {
+          job_title: { $regex: new RegExp(`^${req.query.title}$`, "i") },
+        };
+      }
+
+      // console.log(query);
+      const result = await jobsCollection.find(query).toArray();
+
+      res.send(result);
+    });
 
 
   } finally {
@@ -68,11 +90,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res)=>{
-    res.send('Job server is running')
+app.get("/", (req, res) => {
+  res.send("Job server is running");
 });
 
-app.listen(port, ()=>{
-    console.log(`app is running on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`app is running on port: ${port}`);
+});
