421e0588b211d0b11dd4cbb8fdd4a9bcdbbda3ae
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-09-25 20:55:16 +0600

    edited the delete api function

--- index.js
+++ index.js
@@ -161,9 +161,10 @@ async function run() {
       res.send(result);
     })
 
+    // delete a user
     app.delete('/user/:id', async(req, res)=>{
       const id = req.params.id;
-      const query = {_id: new ObjectId(_id)};
+      const query = {_id: new ObjectId(id)};
       const result = await userCollection.deleteOne(query);
       res.send(result);
     })
